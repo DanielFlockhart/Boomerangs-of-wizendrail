c0e6b24762947bf21274fba571a0157d87e51291
Daniel Flockhart <57047926+DanielFlockhart@users.noreply.github.com>
2020-12-14 23:31:40 +0000

    Update HeathBar.cs

--- HeathBar.cs
+++ HeathBar.cs
@@ -10,7 +10,6 @@ public class HeathBar : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //bar.transform.position = transform.position + new Vector3(0,1f,0);
         GameObject player = GameObject.FindGameObjectWithTag("Ai");
         float percent = player.GetComponent<PlayerScript>().health / 100;
         green.GetComponent<RectTransform>().sizeDelta = new Vector2(104*percent, 12);
