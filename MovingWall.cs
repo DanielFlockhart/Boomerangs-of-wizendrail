12defec9579df2a6340eec8fe8b7ccaeb2f9ae82
Daniel Flockhart <57047926+DanielFlockhart@users.noreply.github.com>
2020-12-14 23:32:33 +0000

    Update MovingWall.cs

--- MovingWall.cs
+++ MovingWall.cs
@@ -7,19 +7,4 @@ public class MovingWall : MonoBehaviour
     public Transform player;
     public Vector3 offset;
     float topMost;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update() {
-
-        //if (player.position.y > topMost)
-        //{
-            //topMost = player.position.y;
-        //}  
-        //transform.position = new Vector3(0, topMost + offset.y, 1);
-    }
 }
