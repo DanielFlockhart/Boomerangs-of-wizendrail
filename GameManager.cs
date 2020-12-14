6ac38b79f683df04b54aa08c4dd5e2378f1615f6
Daniel Flockhart <57047926+DanielFlockhart@users.noreply.github.com>
2020-12-14 23:31:20 +0000

    Update GameManager.cs

--- GameManager.cs
+++ GameManager.cs
@@ -15,8 +15,6 @@ public class GameManager : MonoBehaviour
     {
         loaded_chunks = 5;
     }
-
-    // Update is called once per frame
     void Update() {
     
         if(loaded_chunks >= goal+5)
@@ -28,11 +26,8 @@ public class GameManager : MonoBehaviour
             SceneManager.LoadScene("GamePlay");
         }
     }
-    
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //bar.transform.position = transform.position + new Vector3(0,1f,0);
         GameObject player = GameObject.FindGameObjectWithTag("Ai");
         float percent = loaded_chunks / goal;
         green.GetComponent<RectTransform>().sizeDelta = new Vector2(104 * percent * (104/goal), 12);
